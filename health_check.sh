450decaa3243b5a32a7f4c311c63989b6c47f329
Borys Markiv <brs.markiv@gmail.com>
2022-06-21 22:51:44 -0700

    restore attribs as 1 bit value

---
+++ health_check.sh
@@ -0,0 +1,8 @@
+echo -e "GET http://192.168.1.2 HTTP/1.0\n\n" | nc 127.0.0.1 8080 | grep "200 OK" > nul
+if [ $? -ne 0 ] ; then
+        pkill -f /usr/local/OpenSprinkler/OpenSprinkler -9
+        cd /usr/local/OpenSprinkler && /usr/local/OpenSprinkler/OpenSprinkler &
+        echo $(date "+%Y-%m-%d %H:%M:%S.%3N) OpenSprinkler restarted
+else
+        echo $(date "+%Y-%m-%d %H:%M:%S.%3N) " OK"
+fi
